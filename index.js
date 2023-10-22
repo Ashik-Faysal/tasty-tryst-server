fc3aaf4a67ce5470ed2cf6ff46cede7b3ec441c5
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-10-22 12:17:01 +0600

    added menu and reviews collection

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const port = process.env.PORT || 5000;
 
 // MiddleWare
 app.use(cors());
-
-
-
+app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri =
@@ -27,6 +25,28 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const menuCollection = client.db("tasty-tryst").collection("menu");
+    const reviewsCollection = client.db("tasty-tryst").collection("reviews");
+    
+
+
+    app.get("/menu", async(req, res) => {
+  const result = await menuCollection.find().toArray()
+  res.send(result);
+})
+
+
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+});
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
