42fc38e4d3125408b5f77cd096cdb167795ed66c
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-10-24 00:38:26 +0600

    added admin panel

--- index.js
+++ index.js
@@ -28,6 +28,13 @@ async function run() {
     const reviewsCollection = client.db("tasty-tryst").collection("reviews");
     const cartsCollection = client.db("tasty-tryst").collection("carts");
 
+    // users Api
+    app.get("/users",async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
@@ -38,6 +45,21 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
+
+
+    app.patch("users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+})
+
+
 
     // for menu
     app.get("/menu", async (req, res) => {
